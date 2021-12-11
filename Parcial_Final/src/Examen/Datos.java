12cc1e480a22e24a003bace4e03044706b0d3eca
LuilliGB <94030435+LuilliGB@users.noreply.github.com>
2021-12-10 23:12:36 -0600

    Esqueleto 1
    Se crearon los Jframes del programa

--- Parcial_Final/src/Examen/Datos.java
+++ Parcial_Final/src/Examen/Datos.java
@@ -1,7 +1,54 @@
-
 package Examen;
 
-
 public class Datos {
-    
+
+    String nombre;
+    String apellidos;
+    String Usuario;
+    String contraseña;
+    String correo;
+
+    public Datos() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(String Usuario) {
+        this.Usuario = Usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
 }
